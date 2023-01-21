abf1103f6e9ea646d7a77b499fae838a89849c12
AnwarBassett <abassettma@gmail.com>
2023-01-21 17:39:55 -0600

    ejercicios 60

---
+++ GuiaDeEjercicios/ejercicio17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double kg, lb;
+                do
+                {
+                    //17.	Determinar la energía total que almacena un cuerpo si su energía cinética es
+                    //½ de la masa por su velocidad al cuadrado y la energía potencial
+                    //es el producto de la masa, altura y la constante de gravedad. Recuerde que ET = EC +EP.
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ GuiaDeEjercicios/ejercicio45/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //45 De una lista de n voltajes escriba el programa que calcule el voltaje mínimo,
+                //el voltaje máximo y el promedio de todos.
+
+                Console.ReadKey();
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
